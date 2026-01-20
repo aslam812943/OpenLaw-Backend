de26be1e9d456d784a17ec9e1de734bf85c126f4
Aslam <aslam812943@gmai.com>
2026-01-20 12:44:26 +0530

    refactor: moved app.js file to src.

---
+++ src/app.ts
@@ -0,0 +1,76 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import { Server } from "socket.io";
+
+import userRoutes from "./interface/routes/user/userRoutes";
+import lawyerRoutes from "./interface/routes/lawyer/lawyerRoutes";
+import adminRoutes from "./interface/routes/admin/adminRoutes";
+import bookingRoutes from "./interface/routes/user/bookingRoutes";
+import webhookRoutes from "./interface/routes/user/webhookRoutes";
+import videoCallRoutes from "./interface/routes/common/videoCallRoutes";
+
+import { DbConnection } from "./config/mongoose/DbConnection";
+import { errorHandler } from "./interface/middlewares/errorHandler";
+import { SocketServerService } from "./infrastructure/services/socket/socketServer";
+
+dotenv.config();
+
+const app = express();
+
+
+
+
+DbConnection.connect();
+
+
+app.use(cookieParser());
+
+
+app.use("/api/webhook", express.raw({ type: 'application/json' }), webhookRoutes);
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
+
+//  ROUTES
+
+app.use("/api/user", userRoutes);
+app.use("/api/lawyer", lawyerRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/booking", bookingRoutes);
+app.use("/api/video-call", videoCallRoutes);
+
+
+app.use(errorHandler);
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+  }
+});
+
+
+const socketServerService = new SocketServerService();
+socketServerService.setupSocketServer(io);
+
+
+const PORT = process.env.PORT
+
+server.listen(PORT, () => {
+  console.log(` Server + Socket.IO running on port ${PORT}`);
+});
